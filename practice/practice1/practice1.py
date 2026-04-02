2f1bbcbc9f93dbda630de63257705d892143318e
Kevin Lin <kevinl@cs.uw.edu>
2026-04-01 19:56:25 -0700

    Add practice/practice1

---
+++ practice/practice1/practice1.py
@@ -0,0 +1,92 @@
+# Name: ...
+# CSE 160
+# Spring 2026
+# Programming Practice 1
+
+
+# ~~~ Begin Problem 1 ~~~
+"""
+Print the following three lines of output, paying close attention to spacing and punctuation:
+
+Welcome to CSE 160!
+Data Programming
+Quarter: 26sp
+
+Note: You *must* use the variable named quarter when printing the final line of output. Your code
+should work even if the value of quarter is changed!
+"""
+
+quarter = "26sp"
+
+# Write your code for Problem 1 here!
+
+
+
+# ~~~ End Problem 1 ~~~
+print("~~~~~~~~~~~")
+
+# ~~~ Begin Problem 2 ~~~
+"""
+Use an assignment statement to define a variable named ruths_number with a numerical value of your
+choosing. Then, define a variable named james_number and set it equal to 18.
+
+Using these two variables, write a boolean expression to evaluate whether ruths_number is greater
+than james_number. Print out the value of the expression.
+
+Finally, write a boolean expression to evaluate whether EITHER ruths_number or james_number is
+negative. Print out the value of the expression.
+
+REMEMBER: Your code should work regardless of the numerical value of ruths_number and james_number.
+"""
+
+# Write your code for Problem 2 here!
+
+
+
+# DON'T EDIT THE CODE BELOW
+assert "ruths_number" in locals(), "Make sure to define a variable called ruths_number"
+assert "james_number" in locals(), "Make sure to define a variable called james_number"
+
+# ~~~ End Problem 2 ~~~
+print("~~~~~~~~~~~")
+
+# ~~~ Begin Problem 3 ~~~
+"""
+Given an variable called num_times with an integer value, write a for loop that will count from 1
+to num_times, printing out each number as it is counted.
+
+Also define a variable named total_sum, which should keep track of the sum of the counted numbers
+seen so far. After the loop finishes, total_sum should be equal to the sum of all integers from 1
+to num_times.
+
+REMEMBER: Your code should work even if we change the value of num_times !
+
+== EXAMPLE #1 ==
+num_times = 2
+
+OUTPUT:
+1
+2
+
+== EXAMPLE #2 ==
+num_times = 4
+
+OUTPUT:
+1
+2
+3
+4
+
+"""
+
+num_times = 5
+
+# Write your code for Problem 3 here!
+
+
+
+# DON'T EDIT THE CODE BELOW
+assert "total_sum" in locals(), "Make sure to define a variable called total_sum"
+assert total_sum > num_times, "Make sure total_sum sums together all of the counted numbers"
+
+# ~~~ End Problem 3 ~~~
