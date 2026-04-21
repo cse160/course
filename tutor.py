118c086b9ab355bb5727e0b0ad1789a32257566f
Kevin Lin <kevinl@cs.uw.edu>
2026-04-21 11:35:21 -0700

    tutor.py: Reduce height to 500 and remove noqa

--- tutor.py
+++ tutor.py
@@ -1,4 +1,4 @@
 from urllib.parse import quote
 from IPython.display import IFrame
 
-IFrame("https://pythontutor.com/iframe-embed.html#py=311&code=" + quote(In[-2]), width="100%", height=1000) # noqa
+IFrame("https://pythontutor.com/iframe-embed.html#py=311&code=" + quote(In[-2]), width="100%", height=500)
